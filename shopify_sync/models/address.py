23c7be3ced4344200928e2c1fea3d59cec156cbc
shenry <shenry@handycodejob.com>
2016-04-08 20:51:22 -0700

    edit fields that can actually be null >:(

--- shopify_sync/models/address.py
+++ shopify_sync/models/address.py
@@ -9,20 +9,20 @@ from .base import ShopifyResourceModel
 class Address(ShopifyResourceModel):
     shopify_resource_class = shopify.resources.Address
 
-    address1 = models.CharField(max_length = 256)
-    address2 = models.CharField(max_length = 256)
-    city = models.CharField(max_length = 256)
+    address1 = models.CharField(max_length = 256, null=True)
+    address2 = models.CharField(max_length = 256, null=True)
+    city = models.CharField(max_length = 256, null=True)
     company = models.CharField(max_length = 256, null = True)
-    country = models.CharField(max_length = 256)
-    country_code = models.CharField(max_length = 256)
-    country_name = models.CharField(max_length = 256)
+    country = models.CharField(max_length = 256, null=True)
+    country_code = models.CharField(max_length = 256, null=True)
+    country_name = models.CharField(max_length = 256, null=True)
     default = models.BooleanField(default = False)
     first_name = models.CharField(max_length = 256, null = True)
     last_name = models.CharField(max_length = 256, null = True)
     phone = models.CharField(max_length = 32, null = True)
     province = models.CharField(max_length = 32, null = True)
     province_code = models.CharField(max_length = 32, null = True)
-    zip = models.CharField(max_length = 16, null = True)
+    zip = models.CharField(max_length = 32, null = True)
 
     class Meta:
         app_label = 'shopify_sync'
