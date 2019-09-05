d94234f3be7d00d302d39e5cdad459d129373f44
lucas <lucas@thestrangeagency.com>
2019-09-05 20:43:56 +0000

    do not choke on Receipt objects
     Object of type Receipt is not JSON serializable

--- shopify_sync/encoders.py
+++ shopify_sync/encoders.py
@@ -13,6 +13,8 @@ class ShopifyDjangoJSONEncoder(DjangoJSONEncoder):
     this is a special encoder that handles lazily evaluated strings."""
 
     def default(self, obj):
+        if isinstance(obj, shopify.Receipt):
+            return str(obj)
         if isinstance(obj, shopify.ShopifyResource) and getattr(obj, 'attributes'):
             return obj.attributes
         return super(ShopifyDjangoJSONEncoder, self).default(obj)
